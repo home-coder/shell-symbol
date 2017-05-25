653db760669d21e6a7f51707dad15c99fd4b2144
jiangxiujie <jiangxiujie@cantv.cn>
2017-05-25 14:24:37 +0800

    $0 $n $? $# $* $@

--- yang.sh
+++ yang.sh
@@ -4,11 +4,16 @@ echo $1
 echo $2
 echo $#
 echo $*
-echo "========================="
-#./yang.sh hello wold
-#./yang.sh
-#hello
-#wold
-#2
-#hello wold
+echo $@
+echo $?
+echo $$
 
+echo "========================="
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "First Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
+
+echo "=======@  * 区别=================="
